7344dd161fb19f0f4747a4a319bd8bd6a4cee0a4
harishankar796 <harishankar796@gmail.com>
2022-06-19 17:30:52 +0530

    First Commit

---
+++ src/main/java/com/hari/jenkins/docker/IntegrationGitCicdJenkinsDockerApplication.java
@@ -0,0 +1,21 @@
+package com.hari.jenkins.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationGitCicdJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationGitCicdJenkinsDockerApplication.class, args);
+	}
+
+	
+	/*echo "# integration-git-cicd-jenkins-docker" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/harishankar796/integration-git-cicd-jenkins-docker.git
+	git push -u origin main*/
+}
---
+++ src/test/java/com/hari/jenkins/docker/IntegrationGitCicdJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.jenkins.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationGitCicdJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
